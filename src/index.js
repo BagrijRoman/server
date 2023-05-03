4da2d4e0c27178c872409a9ee17ff4db71020dbd
BagrijRoman <bagrijroman@gmail.com>
2023-05-03 11:29:18 +0300

    setup project

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/test', (req, res) => {
+  res.json('test: Ok!');
+});
+
+const port = process.env.PORT || 4000
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
